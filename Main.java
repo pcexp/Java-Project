e653ec91e532ae183e09d954cbd85ddc485c236f
Oguzhan95275 <oguzhangurcan99@gmail.com>
2024-02-08 19:57:56 +0300

    working space is cleared

--- Main.java
+++ Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is Main Class");
-        System.out.println(5);
-        System.out.println(8);
-        System.out.println("Bu eklentiyi ilk kez goruyosunuz");
     }
 }
