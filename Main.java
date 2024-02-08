10de4a5fe768b3e220869e31185e19cdefd30f88
Oguzhan95275 <oguzhangurcan99@gmail.com>
2024-02-08 19:46:53 +0300

    files are updated2

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Main Class");
         System.out.println(5);
-
+        System.out.println(8);
     }
 }
