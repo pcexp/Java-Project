de2cb9651d1bf66f83d32c76f09325b178475882
pcexp <mehmetcangurcan1@gmail.com>
2024-03-03 18:33:08 +0300

    ufak-tefek-guncelleme-back

--- Main.java
+++ Main.java
@@ -1,18 +1,12 @@
 import java.io.FileNotFoundException;
 import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        List<User> users = Reader.readUser();
+        List<User> users = new Reader().readUser();
         System.out.println(users);
 
 
-
     }
-
-
-
-
 }
--- Reader.java
+++ Reader.java
@@ -1,32 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-
-
 public class Reader {
 
     private static final String USER_FILE_PATH = "data/u.user";
-    private static Scanner scanner;
 
-    public static List<User> readUser() throws FileNotFoundException {
+    public List<User> readUser() throws FileNotFoundException {
 
-        scanner = new Scanner(new File(USER_FILE_PATH));
-        List<User> users = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(USER_FILE_PATH));
+        List<User> userContainer = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] userInfos = line.split("\\|");
-
             Integer id = Integer.parseInt(userInfos[0]);
             Integer age = Integer.parseInt(userInfos[1]);
             Character gender = userInfos[2].charAt(0);
             Integer occupationId = Integer.parseInt(userInfos[3]);
 
             User user = new User(id, age, gender, occupationId);
-            users.add(user);
+            userContainer.add(user);
         }
-        return users;
+        return userContainer;
     }
 
 }
-
