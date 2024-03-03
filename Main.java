7a840046db954a5caa8ed93e9c0eefaf57652d54
pcexp <mehmetcangurcan1@gmail.com>
2024-03-03 18:35:46 +0300

    ufak-tefek-guncelleme-back-2

--- Main.java
+++ Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         List<User> users = new Reader().readUser();
-        System.out.println(users);
+        List<Movie> movies = new Reader().readMovie();
+        List<Occupation> occupations = new Reader().readOccupation();
+        List<Genre> genres = new Reader().readGenre();
+        List<Data> data = new Reader().readData();
 
+        System.out.println(users.get(8).getGender());
+        System.out.println(movies.get(6).getTitle());
+        System.out.println(occupations.get(14).getName());
+        System.out.println(genres.get(8).getGenre());
+        System.out.println(data.get(8).getRate());
 
     }
 }
